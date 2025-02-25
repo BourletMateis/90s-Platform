83efccc7015f4d39a4899da39ee7792b78192ca8
Mateis B <mateisbourlet9@gmail.com>
2025-02-25 10:15:11 +0100

    add controleur

--- Assets/script/controlleur.cs
+++ Assets/script/controlleur.cs
@@ -10,11 +10,16 @@ public class NewBehaviourScript : MonoBehaviour
     public Rigidbody2D rb;
     public float speed = 2f;
     public float jumpForce = 7f;
+    private bool isGrounded;
+    public Transform groundCheck; 
+    public float groundCheckRadius = 0.2f;
+    public LayerMask groundLayer;
+
 
     void Start()
     {
-
         rb = GetComponent<Rigidbody2D>();
+        groundCheck = GameObject.Find("GroundCheck").transform;
     }
 
     void Update()
@@ -26,6 +31,7 @@ public class NewBehaviourScript : MonoBehaviour
     {
         float moveX = 0f;
         float moveY = rb.velocity.y;
+
         if (Input.GetKey(KeyCode.D))
         {
             moveX = speed;
@@ -34,10 +40,22 @@ public class NewBehaviourScript : MonoBehaviour
         {
             moveX = -speed;
         }
-        else if (Input.GetKeyDown(KeyCode.Space))
+
+        if (isGrounded && Input.GetKeyDown(KeyCode.Space))
         {
             moveY = jumpForce;
         }
+
         rb.velocity = new Vector2(moveX, moveY);
+    }
+
+    void FixedUpdate()
+    {
+        OnGround();
+    }
+
+    void OnGround()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
     }
 }
