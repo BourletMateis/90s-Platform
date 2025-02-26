c1dbdf9c3ced11b66bec56ee94efe3d2dcf5ce90
Mateis B <mateisbourlet9@gmail.com>
2025-02-26 19:12:58 +0100

    Merge game menu

--- Assets/script/MainMenuController.cs
+++ Assets/script/MainMenuController.cs
@@ -7,7 +7,7 @@ public class MainMenuController : MonoBehaviour
 {
     public void PlayScene(string sceneName)
     {
-        SceneManager.LoadScene("homePlayer");
+        SceneManager.LoadScene("projet_1_semaine");
     }
 
     public void SettingScene(string sceneName)
